490690684cd488457e53db48bb354e01f7752c53
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-05 17:43:43 +0900

    [AK-17] 전국무료급식소 API 호출 트리거 생성
    +axios 패치

---
+++ index.js
@@ -0,0 +1,19 @@
+let axios = require('axios');
+
+let config = {
+    method: 'get',
+    url: 'http://api.data.go.kr/openapi/tn_pubr_public_free_mlsv_api?serviceKey=b7%2BNGZ' +
+            '%2BxRbFG9ApCFAM3aCNPNwrEvmCyzXzVfIfbaXEpSDY8kSqxnU9j3eUAkZYKaVVzIlZMscxVYmyHKO' +
+            'rarg%3D%3D&pageNo=1&numOfRows=1500&type=json',
+    headers: {
+        'Cookie': 'SCOUTER=x4n12f8688mjeh; clientid=040027336549'
+    }
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
