7041f7b8f28295b44d9d0074b0c6d3108a1a3c06
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-10 18:15:33 +0900

    [AK-17], [AK-40] 내용 수정
    + 서비스 키 노출 삭제
    
    + DB 연결 핸드셰이킹 오류 수정

--- Router/getData.js
+++ Router/getData.js
@@ -6,27 +6,26 @@ const connection = mysql.createConnection(require('../Config/connectDB'));
 
 let config = {
     method: 'get',
-    url: 'http://api.data.go.kr/openapi/tn_pubr_public_free_mlsv_api?serviceKey=b7%2BNGZ' +
-            '%2BxRbFG9ApCFAM3aCNPNwrEvmCyzXzVfIfbaXEpSDY8kSqxnU9j3eUAkZYKaVVzIlZMscxVYmyHKO' +
-            'rarg%3D%3D&pageNo=1&numOfRows=1500&type=json',
+    url: process.env.API_URL,
     headers: {
-        'Cookie': 'SCOUTER=x4n12f8688mjeh; clientid=040027336549'
+        'Cookie': 'SCOUTER=x4n12f8688mjeh; clientid=020097385071'
     }
 };
 
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('mysql connect completed!');
+});
+
 router.get('/', function (req, res) {
-    connection.connect((err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('mysql connect completed!');
-    });
     axios(config)
         .then(function (response) {
             // console.log(JSON.stringify(response.data));
             if (response.data) {
-                res.send('Success get Data!');
+                res.send(JSON.stringify(response.data));
             }
         })
         .catch(function (error) {
