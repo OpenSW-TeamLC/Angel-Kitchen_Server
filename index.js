a92f1fd1453f1460a58c3cbd8bcfd513134f8a5a
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-10 15:33:26 +0900

    [AK-39] 급식소 API 스크립트 라우팅

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const getData = require('./Router/getData');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use(express.json());
+app.use('/getdata', getData);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
