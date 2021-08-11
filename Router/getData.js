0ce5775f8919a54d3f7780477d96020bbed39759
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-11 18:10:31 +0900

    [AK-18] 첫 단위 테스트

--- Router/getData.js
+++ Router/getData.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
 let axios = require('axios');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection(require('../Config/connectDB'));
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('mysql connect completed!');
-});
+// connection.connect((err) => {     if (err) {         console.log(err);
+// return;     }     console.log('mysql connect completed!'); });
 
 let config = {
     method: 'get',
@@ -22,14 +17,39 @@ let config = {
 
 router.get('/', function (req, res) {
     axios(config)
-        .then(function (response) {
+        .then(async (response) => {
             const dataLength = response.data.response.body.items.length;
             console.log(dataLength);
-            // for (let index = 0; index < dataLength; index++) {
-            //     let readData = JSON.stringify(response.data.response.body.items[index]);
-            //     let Data = JSON.parse(readData);
-            //     console.log(Data);
-            // }
+            for (let index = 0; index < dataLength; index++) {
+                let readData = JSON.stringify(response.data.response.body.items[index]);
+                let data = JSON.parse(readData);
+
+                await (await connection).query(
+                    'INSERT INTO kitchen_table(fcltyNm, rdnmadr, lnmadr, phoneNumber, mlsvTrget, ml' +
+                            'svTime, mlsvDate, operOpenDate, latitude, longitude) VALUES(?,?,?,?,?,?,?,?,?,' +
+                            '?)',
+                    [
+                        data.fcltyNm,
+                        data.rdnmadr,
+                        data.lnmadr,
+                        data.phoneNumber,
+                        data.mlsvTrget,
+                        data.mlsvTime,
+                        data.mlsvDate,
+                        data.operOpenDate,
+                        data.latitude,
+                        data.longitude
+                    ],
+                    (err, results, fields) => {
+                        if (err) {
+                            throw err;
+                        }
+                    }
+                );
+            }
+            res
+                .status(201)
+                .json({"status": "success!"});
         })
         .catch(function (error) {
             console.log(error);
