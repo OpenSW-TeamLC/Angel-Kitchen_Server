94411d539f0c5eaa596d22a9e97938d03d2a08f9
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-11 17:09:17 +0900

    [AK-18] API 데이터 index 수 카운트

--- Router/getData.js
+++ Router/getData.js
@@ -16,18 +16,20 @@ let config = {
     method: 'get',
     url: process.env.API_URL,
     headers: {
-        'Cookie': 'SCOUTER=x4n12f8688mjeh; clientid=050097677414'
+        'Cookie': 'SCOUTER=x4n12f8688mjeh; clientid=040051352092'
     }
 };
 
 router.get('/', function (req, res) {
     axios(config)
         .then(function (response) {
-            // console.log(JSON.stringify(response.data));
-            if (response.data) {
-                // res.send(JSON.stringify(response.data));
-                res.send('Get success Data!')
-            }
+            const dataLength = response.data.response.body.items.length;
+            console.log(dataLength);
+            // for (let index = 0; index < dataLength; index++) {
+            //     let readData = JSON.stringify(response.data.response.body.items[index]);
+            //     let Data = JSON.parse(readData);
+            //     console.log(Data);
+            // }
         })
         .catch(function (error) {
             console.log(error);
