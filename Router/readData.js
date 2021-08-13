69fa94ab0ac52b25188e5fe5694f77622440311d
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-13 14:41:11 +0900

    [AK-45] 첫 단위 테스트

--- Router/readData.js
+++ Router/readData.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../Config/connectDB');
 
 router.get('/', async (req, res) => {
-    
+    let result = await(await connection).execute("SELECT * FROM kitchen_table");
+    // console.log(result[0]);
+    res.send(result[0]);
 });
 
 module.exports = router;
