71164d3af5cf2ff2d3b740b38dc6b594a0072b55
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-17 19:51:02 +0900

    [AK-51] Request body 값 확인

--- Router/scanKitchen.js
+++ Router/scanKitchen.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const connection = require('../Config/connectDB');
 
 router.post('/', async (req, res) => {
+    console.log(req.body);
+    res.send(req.body)
 });
 
 module.exports = router;
