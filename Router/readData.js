f3f590a3df6f50105767d933e4762dafbaa00b5f
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-08-22 16:38:01 +0900

    [AK-73] 진행중

--- Router/readData.js
+++ Router/readData.js
@@ -2,6 +2,71 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../Config/connectDB');
 
+/**
+ * @swagger
+ * tags:
+ *   name: readData
+ *   description: kitchen_table DB 전체 조회
+ * definitions:
+ *   readData:
+ *     type: object
+ *     required:
+ *       - fcltyNm
+ *       - latitude
+ *       - longitude
+ *     properties:
+ *       id:
+ *         type: integer
+ *         description: primary key
+ *       fcltyNm:
+ *         type: string
+ *         description: 급식소 이름
+ *       rdnmadr:
+ *         type: string
+ *         description: 급식소 도로명주소
+ *       lnmadr:
+ *         type: string
+ *         description: 급식소 지번주소
+ *       phoneNumber:
+ *         type: string
+ *         description: 급식소 전화번호
+ *       mlsvTrget:
+ *         type: string
+ *         description: 급식 대상
+ *       mlsvTime:
+ *         type: string
+ *         description: 급식 시간
+ *       mlsvDate:
+ *         type: string
+ *         description: 급식 요일
+ *       operOpenDate:
+ *         type: string
+ *         format: date
+ *         description: 급식소 운영시작일자
+ *       latitude:
+ *         type: number
+ *         format: double
+ *         description: 급식소 위도
+ *       longitude:
+ *         type: number
+ *         format: double
+ *         description: 급식소 경도
+ */
+
+/**
+ * @swagger
+ * /readdata:
+ *   get:
+ *     tags: [readData]
+ *     responses:
+ *       200:
+ *         description: 조회 성공 리스트
+ *         schema:
+ *          $ref: '#/definitions/readData'
+ *       400:
+ *         description: 조회 성공 실패 시
+ */
+
 router.get('/', async (req, res) => {
     let result = await(await connection).query("SELECT * FROM kitchen_table");
     // console.log('readData : ', result[0]);
